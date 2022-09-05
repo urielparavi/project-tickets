862933f78f294c8d86a3519569ecbd8b8685c870
Your Name <urielparavi75@gmail.com>
2022-09-05 21:12:43 +0300

    139-new-ticket

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 function NewTicket() {
+  const { user } = useSelector((state) => state.auth);
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
+  const [product, setProduct] = useState('');
+  const [description, setDescription] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <h1>NewTicket</h1>
+    <>
+      <section className="heading">
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below..</p>
+      </section>
+
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Customer Name</label>
+          <input type="text" className="form-control" value={name} disabled />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Customer E-Mail</label>
+          <input type="text" className="form-control" value={email} disabled />
+        </div>
+
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="product">Product</label>
+            <select
+              name="product"
+              id="product"
+              value={product}
+              onChange={(e) => setProduct(e.target.value)}>
+              <option value="Samsung">Samsung</option>
+              <option value="Mobile">Mobile</option>
+              <option value="Tablet">Tablet</option>
+              <option value="Computer">Computer</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description of the issue</label>
+            <textarea
+              name="description"
+              id="description"
+              className='form-control'
+              placeholder='Description'
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
