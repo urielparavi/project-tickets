353a1834b995e7e3cbbdace584fb16cc3a555a6a
Your Name <urielparavi75@gmail.com>
2022-09-10 11:05:46 +0300

    prepare for deploy the present app

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -17,13 +18,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk API' });
-});
-
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/presents', presentRoutes);
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
 
 app.use(errorHandler);
 
